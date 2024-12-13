edd5d093228327dae37629bd3472761942c97517
alexdnf <152063278+alexdnf@users.noreply.github.com>
2024-12-13 15:12:03 +0300

    Update MobileBankApiTestV3.java

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -15,7 +15,7 @@ class MobileBankApiTestV3 {
       .when()
           .get("/demo/accounts")
       .then()
-          .statusCode(200)
+          .statusCode(400)
           .contentType(ContentType.JSON)
           .body("", hasSize(3))
           .body("[0].currency", equalTo("RUB"))
